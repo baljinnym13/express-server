dfabdb52cb54924cafe955e220970a225e3b95e6
baljinnym13 <baljinnym1318@gmail.com>
2024-08-21 12:45:52 +0800

    server asaalaa

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const port = 8000;
+const cors = require("cors");
+
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.use(cors());
+
+const data = fs.readFileSync("./users.json", { encoding: "utf8" });
+const { users } = JSON.parse(data);
+
+app.get("/users", (req, res) => {
+  res.status(200).json({ users: users });
+});
+
+app.post("/users", (req, res) => {
+  const newuser = {
+    id: users.length + 1,
+    name: req.body.name,
+    age: req.body.age,
+  };
+  users.push(newuser);
+  fs.writeFileSync("./users.json", JSON.stringify({ users }));
+  res.status(201).json({ user: newuser });
+});
+
+app.put("/users/:userId", (req, res) => {
+  const findIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.userId)
+  );
+  if (findIndex > -1) {
+    users[findIndex].name = req.body.name;
+    fs.writeFileSync("./users.json", JSON.stringify({ users }));
+
+    res.status(200).json({ user: users[findIndex] });
+  } else {
+    res.status(400).json({ message: "not found user id" });
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const findIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.id)
+  );
+  if (findIndex > -1) {
+    const deletedUser = users.splice(findIndex, 1);
+    fs.writeFileSync("./users.json", JSON.stringify({ users }));
+    res.status(200).json({ user: deletedUser[0] });
+  } else {
+    res.status(400).json({ message: "not found user id" });
+  }
+});
+
+app.listen(port, () => {
+  console.log("server is runing at localhost:8000");
+});
